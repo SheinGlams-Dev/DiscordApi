8f337dd9f28b971857e5e4e89862277d0f459e6d
SheinDev <sheindev2024@gmail.com>
2024-07-08 22:47:06 +0700

    Update avatar.js

--- routes/avatar.js
+++ routes/avatar.js
@@ -3,60 +3,36 @@ const axios = require("axios");
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
-    const userId = req.params.id;
+router.get("/:id", async (req, res) => {
+  const userId = req.params.id;
 
-    axios({
-      method: "GET",
-      url: `https://discord.com/api/v9/users/${userId}/profile`,
+  try {
+    const response = await axios.get(`https://discord.com/api/v9/users/${userId}/profile`, {
       headers: {
         "authorization": process.env.TOKEN,
       },
-    })
-      .then(response => {
-        const avatarId = response.data.user.avatar;
-  
-        if (!avatarId) {
-          res.status(404).send("El usuario no tiene banner");
-          return;
-        }
-  
-        axios({
-          method: "GET",
-          url: `https://cdn.discordapp.com/avatars/${userId}/${avatarId}?size=2048`,
-          responseType: "arraybuffer",
-          headers: {
-            "Content-Type": "image/*",
-          },
-        })
-          .then(response => {
-            if (response.status !== 200) {
-              throw new Error(`Network response was not ok. Status: ${response.status} ${response.statusText}`);
-            }
-  
-            const contentType = response.headers["content-type"];
-            let extension = "webp";
-  
-            if (contentType === "image/gif") {
-              extension = "gif";
-            } else if (contentType === "image/png") {
-              extension = "png";
-            } else if (contentType === "image/jpeg" || contentType === "image/jpg") {
-              extension = "jpg";
-            }
-  
-            res.set("Content-Type", contentType);
-            res.send(response.data);
-          })
-          .catch(error => {
-            console.error("Error en la petición:", error.message);
-            res.status(500).send("Error al obtener la imagen del usuario");
-          });
-      })
-      .catch(error => {
-        console.error("Error en la petición:", error.message);
-        res.status(500).send("Error al obtener el perfil del usuario");
-      });
+    });
+
+    const avatarId = response.data.user.avatar;
+
+    if (!avatarId) {
+      return res.status(404).send("El usuario no tiene avatar");
+    }
+
+    const avatarResponse = await axios.get(`https://cdn.discordapp.com/avatars/${userId}/${avatarId}?size=2048`, {
+      responseType: "arraybuffer",
+      headers: {
+        "Content-Type": "image/*",
+      },
+    });
+
+    const contentType = avatarResponse.headers["content-type"];
+    res.set("Content-Type", contentType);
+    res.send(avatarResponse.data);
+  } catch (error) {
+    console.error("Error en la petición:", error.message);
+    res.status(500).send("Error al obtener el avatar del usuario");
+  }
 });
 
 module.exports = router;
