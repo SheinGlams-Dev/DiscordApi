19b0c2e88ac872da6d3eda9d2babf2e0ce6b2ba6
SheinDev <sheindev2024@gmail.com>
2024-07-08 22:48:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
-const cors = require('cors');
 const port = 4000;
-
-// let allowedOrigins = [
-//   'https://localhost:3000',
-//   'http://localhost:3000',
-// ]
 
 app.use(cors());
 app.use(express.json());
@@ -18,13 +13,14 @@ const { profile, avatar, banner, badge } = require("./routes/routes");
 
 // Default route
 app.get("/", (req, res) => {
-  res.send("Selamat datang di API untuk mendapatkan profil, avatar, dan spanduk pengguna Discord, gunakan /api/profile/:id, /api/avatar/:id atau /api/banner/:id untuk mendapatkan informasi");
+  res.send("Selamat datang di API para obtener el perfil, avatar y banner del usuario de Discord. Use /api/profile/:id, /api/avatar/:id, o /api/banner/:id para obtener información.");
 });
+
 app.use('/api/profile', profile);
 app.use('/api/avatar', avatar);
 app.use('/api/banner', banner);
 app.use('/api/badge', badge);
 
 app.listen(port, () => {
-  console.log(`API sedang running di port ${port}`);
+  console.log(`API está funcionando en el puerto ${port}`);
 });
