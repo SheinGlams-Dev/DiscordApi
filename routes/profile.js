1b818f8347ac46952b3825fec3ff84a00e5a6698
SheinDev <sheindev2024@gmail.com>
2024-07-08 22:48:20 +0700

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -3,22 +3,20 @@ const axios = require("axios");
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const userId = req.params.id;
 
-    axios({
-        method: "GET",
-        url: `https://discord.com/api/v9/users/${userId}/profile`,
-        headers: {
-            "authorization": process.env.TOKEN,
-        },
-    }).then(response => {
-        return res.send(response.data);
-    })
-    .catch(error => {
+    try {
+        const response = await axios.get(`https://discord.com/api/v9/users/${userId}/profile`, {
+            headers: {
+                "authorization": process.env.TOKEN,
+            },
+        });
+        res.send(response.data);
+    } catch (error) {
         console.error("Error en la petición:", error.message);
-        return res.status(500).send("Error al obtener el perfil del usuario");
-    });
+        res.status(500).send("Error al obtener el perfil del usuario");
+    }
 });
 
 module.exports = router;
