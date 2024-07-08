6d8926d4fec825e3e710485359b6ab6827818eb6
SheinDev <sheindev2024@gmail.com>
2024-07-08 22:47:31 +0700

    Update badge.js

--- routes/badge.js
+++ routes/badge.js
@@ -3,39 +3,24 @@ const axios = require("axios");
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const badgeId = req.params.id;
 
-    axios({
-        method: "GET",
-        url: `https://cdn.discordapp.com/badge-icons/${badgeId}`,
-        responseType: "arraybuffer",
-        headers: {
-            "Content-Type": "image/*",
-        },
-    }).then(response => {
-        if (response.status !== 200) {
-            throw new Error(`Network response was not ok. Status: ${response.status} ${response.statusText}`);
-        }
+    try {
+        const response = await axios.get(`https://cdn.discordapp.com/badge-icons/${badgeId}`, {
+            responseType: "arraybuffer",
+            headers: {
+                "Content-Type": "image/*",
+            },
+        });
 
         const contentType = response.headers["content-type"];
-        let extension = "webp";
-
-        if (contentType === "image/gif") {
-            extension = "gif";
-        } else if (contentType === "image/png") {
-            extension = "png";
-        } else if (contentType === "image/jpeg" || contentType === "image/jpg") {
-            extension = "jpg";
-        }
-
-        // Enviar la imagen como respuesta en la API
         res.set("Content-Type", contentType);
         res.send(response.data);
-    }).catch(error => {
+    } catch (error) {
         console.error("Error en la petición:", error.message);
         res.status(500).send("Error al obtener el badge icon");
-    });
+    }
 });
 
 module.exports = router;
